9344c6c2052ef0ae57af16448873e2fb48cd0651
aeryz <abdullaheryz@protonmail.com>
2021-01-20 10:29:55 +0300

    feat(lexer): add operators with tests

---
+++ src/lexer.rs
@@ -0,0 +1,320 @@
+//! Lexer (a.k.a. Tokenizer) of the Nasl language.
+//!
+//! For now, it supports unicode but it might be changed
+//! in the future for performance reasons. We already know
+//! that the original interpreter uses Latin encoding so
+//! we don't need to pay for unicode.
+
+use crate::token::Token;
+use std::str::Chars;
+
+type LResult<T> = Result<T, String>;
+
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    data: &'a str,
+
+    // Using an iterator here because immediate access does not exist
+    // in the unicode.
+    peek_cursor: Chars<'a>,
+
+    cur_pos: usize,
+    cur_char: Option<char>,
+    peek_char: Option<char>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(data: &'a str) -> Self {
+        let mut peek_cursor = data.chars();
+        peek_cursor.next();
+        peek_cursor.next();
+        Lexer {
+            data,
+            peek_cursor,
+            cur_pos: 0,
+            cur_char: data.chars().next(),
+            peek_char: data.chars().skip(1).next(),
+        }
+    }
+
+    pub fn next_token(&mut self) -> LResult<Token<'a>> {
+        self.eat_whitespace_or_comment();
+        let token = match self.cur_char {
+            Some('(') => Token::Lparan,
+            Some(')') => Token::Rparan,
+            Some('{') => Token::Lbrace,
+            Some('}') => Token::Rbrace,
+            Some('[') => Token::Lbracket,
+            Some(']') => Token::Rbracket,
+            Some(';') => Token::SemiColon,
+            Some(':') => Token::Colon,
+            Some('"') => Token::Str(self.read_str('"')?),
+            Some('\'') => Token::Str(self.read_str('\'')?),
+            Some(',') => Token::Comma,
+            Some('+') => {
+                let token = match self.peek_char {
+                    Some('+') => Token::Incr,
+                    Some('=') => Token::PlusEq,
+                    _ => Token::Plus,
+                };
+                if token != Token::Plus {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('-') => {
+                let token = match self.peek_char {
+                    Some('-') => Token::Decr,
+                    Some('=') => Token::MinusEq,
+                    _ => Token::Minus,
+                };
+                if token != Token::Minus {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('*') => {
+                let token = match self.peek_char {
+                    Some('*') => Token::Pow,
+                    Some('=') => Token::MulEq,
+                    _ => Token::Mul,
+                };
+                if token != Token::Mul {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('/') => {
+                let token = match self.peek_char {
+                    Some('=') => Token::DivEq,
+                    _ => Token::Div,
+                };
+                if token != Token::Div {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('%') => {
+                let token = match self.peek_char {
+                    Some('=') => Token::ModEq,
+                    _ => Token::Mod,
+                };
+                if token != Token::Mod {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('=') => {
+                let token = match self.peek_char {
+                    Some('=') => Token::Equ,
+                    Some('~') => Token::ReMatch,
+                    _ => Token::Assign,
+                };
+                if token != Token::Assign {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('!') => {
+                let token = match self.peek_char {
+                    Some('=') => Token::Nequ,
+                    Some('~') => Token::NreMatch,
+                    _ => Token::Not,
+                };
+                if token != Token::Not {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some('&') => {
+                if self.peek_char == Some('&') {
+                    let _ = self.read_char();
+                    Token::And
+                } else {
+                    Token::BwAnd
+                }
+            }
+            Some('|') => {
+                if self.peek_char == Some('|') {
+                    let _ = self.read_char();
+                    Token::Or
+                } else {
+                    Token::BwOr
+                }
+            }
+            Some('^') => {
+                if self.peek_char == Some('=') {
+                    let _ = self.read_char();
+                    Token::XorEq
+                } else {
+                    Token::BwXor
+                }
+            }
+            Some('>') => {
+                let token = match self.peek_char {
+                    Some('<') => {
+                        let _ = self.read_char();
+                        Token::Substr
+                    }
+                    Some('=') => {
+                        let _ = self.read_char();
+                        Token::Gte
+                    }
+                    Some('>') => {
+                        let _ = self.read_char();
+                        let tok = match self.peek_char {
+                            Some('=') => Token::ShrEq,
+                            Some('>') => Token::Ushr,
+                            _ => Token::Shr,
+                        };
+                        if tok != Token::Shr {
+                            let _ = self.read_char();
+                        }
+                        tok
+                    }
+                    Some('!') => {
+                        let _ = self.read_char();
+                        let tok = if self.peek_char == Some('<') {
+                            let _ = self.read_char();
+                            Token::NSUBSTR
+                        } else {
+                            return Err(format!("Expected '<', got {:?}", self.peek_char));
+                        };
+                        let _ = self.read_char();
+                        tok
+                    }
+                    _ => Token::Gt,
+                };
+                token
+            }
+            Some('<') => {
+                let token = match self.peek_char {
+                    Some('=') => Token::Lte,
+                    Some('<') => {
+                        let _ = self.read_char();
+                        match self.peek_char {
+                            Some('=') => {
+                                let _ = self.read_char();
+                                Token::ShlEq
+                            }
+                            _ => Token::Shl,
+                        }
+                    }
+                    _ => Token::Lt,
+                };
+                if token != Token::Lt {
+                    let _ = self.read_char();
+                }
+                token
+            }
+            Some(ch) => {
+                if ch.is_ascii_alphabetic() || ch == '_' {
+                    let key = self.read_identifier();
+                    return Ok(Token::from_ident(key));
+                } else if ch.is_numeric() {
+                    let key = self.read_number()?;
+                    return Ok(Token::Num(key));
+                }
+                return Err(format!("Unexpected character {}", ch));
+            }
+            None => Token::Eof,
+        };
+        let _ = self.read_char();
+        Ok(token)
+    }
+}
+
+impl<'a> Lexer<'a> {
+    fn read_str(&mut self, ind: char) -> LResult<&'a str> {
+        let _ = self.read_char();
+        let cur_pos = self.cur_pos;
+        while let Some(ch) = self.cur_char {
+            if ch == ind {
+                return Ok(&self.data[cur_pos..self.cur_pos]);
+            }
+            let _ = self.read_char();
+        }
+        Err(String::from(":("))
+    }
+
+    fn eat_whitespace_or_comment(&mut self) {
+        let mut in_comment = false;
+        while let Some(ch) = self.cur_char {
+            if in_comment {
+                if ch == '\n' {
+                    in_comment = false;
+                }
+                let _ = self.read_char();
+                continue;
+            }
+            if ch == '#' {
+                in_comment = true;
+                continue;
+            }
+            if ch != '\t' && ch != '\r' && ch != '\x0C' && ch != ' ' && ch != '\n' {
+                break;
+            }
+            let _ = self.read_char();
+        }
+    }
+
+    fn read_char(&mut self) -> Option<char> {
+        self.cur_char = self.peek_char;
+        self.peek_char = self.peek_cursor.next();
+        self.cur_pos += 1;
+        self.cur_char
+    }
+
+    fn read_identifier(&mut self) -> &'a str {
+        let cur_pos = self.cur_pos;
+        while let Some(ch) = self.cur_char {
+            if !ch.is_ascii_alphanumeric() && ch != '_' {
+                break;
+            }
+            let _ = self.read_char();
+        }
+        &self.data[cur_pos..self.cur_pos]
+    }
+
+    fn read_number(&mut self) -> LResult<i32> {
+        let cur_pos = self.cur_pos;
+        while let Some(ch) = self.cur_char {
+            if !ch.is_numeric() {
+                break;
+            }
+            let _ = self.read_char();
+        }
+        self.data[cur_pos..self.cur_pos]
+            .parse::<i32>()
+            .map_err(|_| {
+                format!(
+                    "{} is not a valid number.",
+                    &self.data[cur_pos..self.cur_pos]
+                )
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn operators_pass() {
+        let ops = [
+            "+", "-", "*", "/", "%", "=", "==", "!=", "&&", "||", ">", "<", ">=", "<=", "!", "&",
+            "|", "^", "+=", "-=", "/=", "%=", "*=", "^=", ">>", "<<", ">>=", "<<=", ">>>", "++",
+            "--", "**", "><", ">!<", "=~", "!~",
+        ];
+        let data = ops.join(" ");
+        let mut lexer = Lexer::new(data.as_str());
+        let mut ops_iter = ops.iter();
+        while let Ok(tok) = lexer.next_token() {
+            if tok == Token::Eof {
+                break;
+            }
+            assert_eq!(Token::from_str(ops_iter.next().unwrap()), tok);
+        }
+        assert_eq!(ops_iter.next(), None);
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,114 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token<'a> {
+    /* Operators */
+    Plus,     // +
+    Minus,    // -
+    Mul,      // *
+    Div,      // /
+    Mod,      // %
+    Assign,   // =
+    Equ,      // ==
+    Nequ,     // !=
+    And,      // &&
+    Or,       // ||
+    Gt,       // >
+    Lt,       // <
+    Gte,      // >=
+    Lte,      // <=
+    Not,      // !
+    BwAnd,    // &
+    BwOr,     // |
+    BwXor,    // ^
+    PlusEq,   // +=
+    MinusEq,  // -=
+    DivEq,    // /=
+    ModEq,    // %=
+    MulEq,    // *=
+    XorEq,    // ^=
+    Shr,      // >>
+    Shl,      // <<
+    ShrEq,    // >>=
+    ShlEq,    // <<=
+    Ushr,     // >>>
+    Incr,     // ++
+    Decr,     // --
+    Pow,      // **
+    Substr,   // ><
+    NSUBSTR,  // >!<
+    ReMatch,  // =~
+    NreMatch, // !~
+
+    If,
+    Else,
+    Lparan,
+    Rparan,
+    Lbrace,
+    Rbrace,
+
+    Lbracket,
+    Rbracket,
+
+    SemiColon,
+    Comma,
+    Ident(&'a str),
+    Str(&'a str),
+    Colon,
+
+    Num(i32),
+
+    Eof,
+    Comment,
+}
+
+impl<'a> Token<'a> {
+    pub fn from_ident(data: &'a str) -> Self {
+        match data {
+            "if" => Token::If,
+            "else" => Token::Else,
+            _ => Token::Ident(data),
+        }
+    }
+
+    // TODO: This only supports operators
+    pub fn from_str(data: &'a str) -> Self {
+        match data {
+            "+" => Token::Plus,
+            "-" => Token::Minus,
+            "*" => Token::Mul,
+            "/" => Token::Div,
+            "%" => Token::Mod,
+            "=" => Token::Assign,
+            "==" => Token::Equ,
+            "!=" => Token::Nequ,
+            "&&" => Token::And,
+            "||" => Token::Or,
+            ">" => Token::Gt,
+            "<" => Token::Lt,
+            ">=" => Token::Gte,
+            "<=" => Token::Lte,
+            "!" => Token::Not,
+            "&" => Token::BwAnd,
+            "|" => Token::BwOr,
+            "^" => Token::BwXor,
+            "+=" => Token::PlusEq,
+            "-=" => Token::MinusEq,
+            "/=" => Token::DivEq,
+            "%=" => Token::ModEq,
+            "*=" => Token::MulEq,
+            "^=" => Token::XorEq,
+            ">>" => Token::Shr,
+            "<<" => Token::Shl,
+            ">>=" => Token::ShrEq,
+            "<<=" => Token::ShlEq,
+            ">>>" => Token::Ushr,
+            "++" => Token::Incr,
+            "--" => Token::Decr,
+            "**" => Token::Pow,
+            "><" => Token::Substr,
+            ">!<" => Token::NSUBSTR,
+            "=~" => Token::ReMatch,
+            "!~" => Token::NreMatch,
+            _ => panic!("Unknown token {}", data),
+        }
+    }
+}
